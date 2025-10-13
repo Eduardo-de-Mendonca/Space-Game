0524bc37d00d9d41f89b8aed4d6df52616185120
EduMFreire <eduardodemendoncafreire@gmail.com>
2025-10-13 20:45:16 -0300

    Adiciona dois arquivos de texto com ideias.
    Um para enviar ao professor como a entrega da semana.
    Outro para discutirmos entre nós.

--- classes.py
+++ classes.py
@@ -1,16 +1,19 @@
 import pygame
 
 class StartScreen:
-    def __init__(self, screen):
-        self.screen = screen
+    def __init__(self, surface):
+        self.surface = surface # Uma referência para a "superfície" do pygame, onde faremos os desenhos
 
     def tick(self):
-        assert isinstance(self.screen, pygame.Surface)
+        # Toda tela tem um método tick, que é chamado todo frame em main.py
 
+        assert isinstance(self.surface, pygame.Surface) # Para o highlighting de sintaxe funcionar direitinho nos métodos de self.surface. Esse "assert isinstance" é algo de que devemos usar e abusar ao longo do projeto, pois ele evita erros de execução e sugere boas práticas na hora de organizar o código
+
+        # Exemplo: desenhar um círculo
         color1 = pygame.Color(200, 200, 200)
         color2 = pygame.Color(100, 100, 100)
         center = pygame.Vector2(100, 100)
         radius = 50
 
-        self.screen.fill(color1)
-        pygame.draw.circle(self.screen, color2, center, radius)
+        self.surface.fill(color1)
+        pygame.draw.circle(self.surface, color2, center, radius)

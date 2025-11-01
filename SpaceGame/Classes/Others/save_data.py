6e74deec7f1fb505bb4e0d7cb438cb17d631cad6
Carlos <carlosaugusto.bertao@gmail.com>
2025-10-31 22:44:01 -0300

    d

--- SpaceGame/Classes/Others/save_data.py
+++
@@ -1,9 +0,0 @@
-from Classes.PlanetSurfaceStuff.planet import Planet
-
-class SaveData:
-    '''
-    Guarda todos os dados que planejamos salvar entre sessões de jogo. Saves ainda não estão implementados, mas é bom já ir guardando aqui em vez de usar variáveis globais.
-    '''
-
-    def __init__(self):
-        self.all_planets = [Planet()]
